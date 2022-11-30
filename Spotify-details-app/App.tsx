a7e8b3030398b76c09130877f70c7020a55f2f47
BuildTools <unconfigured@null.spigotmc.org>
2022-11-30 17:19:01 +0100

    Current track updated

--- Spotify-details-app/App.tsx
+++ Spotify-details-app/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, Image } from 'react-native';
 import { ResponseType, useAuthRequest } from 'expo-auth-session';
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
@@ -7,13 +7,15 @@ import { useEffect, useState } from 'react';
 import axios,* as others from 'axios';
 const Stack = createNativeStackNavigator();
 
+
+
 let accessToken = ""
 export function Login() {
   const navigation : any = useNavigation();
 
   const client_Id:string = "a2aab0598b1547f4b2f9fe66828e8ebc";
   const client_Secret:string = "d02846da1f8d4759be3cca85d186f9db";
-  const redirect_Uri:string = "exp://172.16.192.177:19000"
+  const redirect_Uri:string = "exp://192.168.0.211:19000"
 
   const discovery = {
     authorizationEndpoint : "https://accounts.spotify.com/authorize",
@@ -39,7 +41,6 @@ export function Login() {
     if (response?.type === "success") {
       const{access_token} = response.params;
       accessToken = access_token;
-      console.log(accessToken)
     }
   }, [response])
   return (
@@ -49,30 +50,44 @@ export function Login() {
     </View>
   )
 }
-
-export function Home() {
-  const navigation : any = useNavigation();
+export function CurrentTrack() {
   const [currentSong, setCurrentSong] = useState<any>();
   useEffect(()=> {
     
     const getCurrentTrack = async () => {
       let track;
       try {
-        console.log("Current Track log :"+accessToken)
         track = await axios.get(`https://api.spotify.com/v1/me/player/currently-playing`, {headers: {'Authorization': `Bearer ${accessToken}`,'Content-Type' : 'application/json'}});
       } catch (error) {
         console.log("Computer says no")
       }
       setCurrentSong(track.data)
-      console.log(track.data)
+      console.log(track.data.item.artists[0])
     }
     getCurrentTrack()
   },[])
 
   return (
     <View>
+      {!currentSong ?
+       <Text>Computer says no</Text>:
+       <View>
+          <Image style={styles.albumCover} source={{uri:currentSong.item.album.images[0].url}}/>
+          <Text>{currentSong?.item.name} – {currentSong?.item.artists[0].name}</Text>
+          <Text>
+            Hellos
+          </Text>
+      </View>
+      }
+    </View>
+  )
+}
+export function Home() {
+  const navigation : any = useNavigation();
+  return (
+    <View>
       <Button title="Login" onPress={() => navigation.navigate("Login")}/>
-      {!currentSong ? <Text>Computer says no</Text>:<Text>{currentSong?.item.name}</Text>}
+      <CurrentTrack/>
     </View>
   )
 }
@@ -98,4 +113,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  albumCover : {
+    height: 80,
+    width:80
+  }
 });
