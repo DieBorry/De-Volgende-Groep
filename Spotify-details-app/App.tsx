6762b579089458b07088a2be44fc89dc2527c9e4
BuildTools <unconfigured@null.spigotmc.org>
2022-11-30 17:19:16 +0100

    Current track updated

--- Spotify-details-app/App.tsx
+++ Spotify-details-app/App.tsx
@@ -56,8 +56,10 @@ export function CurrentTrack() {
     
     const getCurrentTrack = async () => {
       let track;
+      let artist;
       try {
         track = await axios.get(`https://api.spotify.com/v1/me/player/currently-playing`, {headers: {'Authorization': `Bearer ${accessToken}`,'Content-Type' : 'application/json'}});
+        artist = await axios.get(``, {headers: {'Authorization': `Bearer ${accessToken}`,'Content-Type' : 'application/json'}});
       } catch (error) {
         console.log("Computer says no")
       }
